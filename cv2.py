7d0dd680cbb07fca2cffeb15bf267581ea496a02
kaba201935-sketch <kaba201935@gmail.com>
2026-02-18 14:34:38 +0100

    Add files via upload

---
+++ cv2.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+with st.sidebar:
+    st.image("Am.JPG",width=100)
+    st.title("Amadou")
+    st.title("kaba")
+    st.write("Adresse: Dakar point E")
+    st.write("Email: kaba201935@gmail.con")
+    st.write("Telephone:783119574")
+    st.write("Nationalite:senegalaise")
+
+
+
+#
+# CONTENU LOCAL
+#
+st.header("Profil")
+st.write("Sériesee motivée et dynamique je suis à la recherche d'un stage afin de mettre en pratique mes connaissances théoriquement")
+st.write("compétences technique,.Organisée et impliquée, je fais preuves d'un bon esprit d'apprentissage et de collaboration, avec une exellente capacite")
+st.write("Mon objectif est de contribuer efficacement aux activités de votre structure tout en acquérant une expérence professionnelle enrichissant")
+#Formations#
+st.header("Formations")
+st.write("👉BTS en Geomatique/ CEDT:G15")
+st.write("👉Licence en Geographie /UNIVERSITE CHEUKH ANTA DIOP (UCAD)")
+st.write("👉Baccalaureat /Groupe scolaire le Bentenier")
+#competences#
+st.header("Competences")
+st.write("👉Notion de base en Système d'Information Géographique")
+st.write("👉Saisie organisation et mise à jour de données géographique")
+st.write("👉Création et lecture de cartes thématiques")
+st.write("👉Notions de topographie et des levés simples")
+st.write("👉Manipulation des données spatial")
+st.write("👉Introduction à la télédétection et aux images satélittes")
+st.write("👉Bases en géodésie et systèmes de coordonnées")
